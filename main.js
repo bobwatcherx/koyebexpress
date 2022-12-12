c483e854b751fb445daf25b3cf9aaf271f961dcd
root <bobwatcherx@gmail.com>
2022-12-12 08:50:15 +0700

    push

---
+++ main.js
@@ -0,0 +1,12 @@
+
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello From Koyeb!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
